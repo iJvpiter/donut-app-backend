a5bd1ea614ed7b4049440d16cfc143126eb70daf
iJvpiter <rocketfx896@gmail.com>
2025-06-06 09:06:31 -0600

    First commit

---
+++ src/main/java/com/example/donutapp3a/DonutApp3aApplication.java
@@ -0,0 +1,13 @@
+package com.example.donutapp3a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutApp3aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutApp3aApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/donutapp3a/DonutApp3aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donutapp3a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutApp3aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
